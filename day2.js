894a9f2bd0818071a185b4a63afe3aa72e3d517b
Marcelo Palacios <jpdjcp@gmail.com>
2025-02-26 11:39:15 -0300

    Modified day2.js: resolution challenge of day 2

--- day2.js
+++ day2.js
@@ -3,3 +3,19 @@ let age = prompt("¿Cuántos años tienes?");
 let language = prompt("¿Qué lenguaje de programación estás estudiando?");
 
 alert(`Hola ${name}, tienes ${age} años y ya estás aprendiendo ${language}!`);
+
+// ejercicio opcional
+ask();
+
+function ask() {
+    let answer = prompt(`¿Te gusta estudiar ${language}? Responde con el número 1 para SÍ o 2 para NO.`);
+
+    if (answer == 1) {
+        alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+    } else if (answer == 2) {
+        alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?");
+    } else {
+        alert("Debe ingresar 1 para SÍ o 2 para NO");
+        ask();
+    }
+}
