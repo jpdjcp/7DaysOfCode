4667d9b99561f7ab4fa932fdde14d33f3a989bf7
Marcelo Palacios <jpdjcp@gmail.com>
2025-02-26 11:00:13 -0300

    Created: day2.js

---
+++ day2.js
@@ -0,0 +1,5 @@
+let name = prompt("¿Cuál es tu nombre?");
+let age = prompt("¿Cuántos años tienes?");
+let language = prompt("¿Qué lenguaje de programación estás estudiando?");
+
+alert(`Hola ${name}, tienes ${age} años y ya estás aprendiendo ${language}!`);
