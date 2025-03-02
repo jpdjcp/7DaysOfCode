6f0d06f988837ab3431c2d29131279cecae27c8e
Marcelo Palacios <jpdjcp@gmail.com>
2025-03-02 13:30:57 -0300

    day6 challenge

--- day5.js
+++ day5.js
@@ -14,9 +14,13 @@ function shopping() {
         shopByCategory();
         shopping();
     } else if(goShopping === "no") {
-        alert('Lista de compras:\nFrutas: ' + fruits.join(", ")
-                            + '\nVerduras: ' + vegetables.join(", ")
-                            + '\nCarnes: ' + meats.join(", "));
+        if (!emptyLists()) {
+            let deleteItem = prompt('¿Deseas eliminar un alimento de tu lista de compras? \n Debes responder con "si" o "no"');
+            if(deleteItem === "si") {
+                deleteItemFromList();
+            }
+        }
+        showLists();
     } else {
         alert('Debes responder con "si" o "no"');
         shopping();
@@ -37,4 +41,30 @@ function shopByCategory() {
         alert('Debes responder con un número válido');
         shopByCategory();
     }
+}
+
+function deleteItemFromList() {
+    showLists();
+    let category = prompt('¿De qué categoría deseas eliminar un alimento de tu lista de compras? \n 1 - Frutas, 2 - Verduras, 3 - Carnes');
+    let item = prompt('¿Qué alimento deseas eliminar de tu lista de compras?');
+
+    if(category === "1") {
+        fruits = fruits.filter(fruit => fruit !== item);
+    } else if(category === "2") {
+        vegetables = vegetables.filter(vegetable => vegetable !== item);
+    } else if(category === "3") {
+        meats = meats.filter(meat => meat !== item);
+    } else {
+        alert('Debes responder con un número válido');
+    }
+}
+
+function showLists() {
+    alert('Lista de compras:\nFrutas: ' + fruits.join(", ")
+                            + '\nVerduras: ' + vegetables.join(", ")
+                            + '\nCarnes: ' + meats.join(", "));
+}
+
+function emptyLists() {
+    return fruits.length === 0 && vegetables.length === 0 && meats.length === 0;
 }
